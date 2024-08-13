f2f526532ba050eb70e8d0d9f5a20bd2521a21bc
Gimme-Wings <rgorelik2022@fau.edu>
2024-08-13 22:18:18 +0000

    Tue, Aug 13, 2024, 10:18 PM +00:00

--- calc.c
+++ calc.c
@@ -1,20 +1,20 @@
 #include <cs50.h>
 #include <stdio.h>
-int add (void)
+int add (int a, int b);
 int main(void)
 {
     int x =get_int("x; ");
     int y = get_int("y; ");
 
-    int z= add();
+    int z= add(x, y);
 
 printf("%i\n", z);  //printf("x+y\n") is to literrally print what is between the quotes so %i is used for integers and the like
 
 
 
 }
-int add (void)//the first int in this line is the return value
+int add (int a, int b)//the first int in this line is the return value
 {
-return x+y;
+return a+b ;
 
 }
