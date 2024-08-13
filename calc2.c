6592b87261c6449ae2e1e4f89f0e7ce598f9f6eb
Gimme-Wings <rgorelik2022@fau.edu>
2024-08-13 23:34:02 +0000

    Tue, Aug 13, 2024, 11:34 PM +00:00

--- calc2.c
+++ calc2.c
@@ -3,10 +3,10 @@
 
 int main (void)
 {
-    float x= get_float("x; ");
-    float y = get_float ("y;  ");
-
-    printf("%f\n", x / y);
+    int x= get_int("x; ");
+    int y = get_int ("y;  ");
+float z = (float) x/(float) y;
+    printf("%f\n", z);
 
 
 }
