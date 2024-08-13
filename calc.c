1666deeb1d72fbaa6b5459faa20a307c39c620e2
Gimme-Wings <rgorelik2022@fau.edu>
2024-08-13 22:28:10 +0000

    Tue, Aug 13, 2024, 10:28 PM +00:00

--- calc.c
+++ calc.c
@@ -3,14 +3,12 @@
 int add (int a, int b);
 int main(void)
 {
-    int x = get_int("x; ");
-    int y = get_int("y; ");
+    int x = get_int("x= ");
+    int y = get_int("y= ");
 
-    int z= add(x, y);
-
-printf("%i\n", z);  //printf("x+y\n") is to literrally print what is between the quotes so %i is used for integers and the like
-
-
+   // this can be used on this line int z= add(x, y);
+//but i will add z= function as a replacement below inside the parentheseses
+printf("%i\n",  add(x, y));  //printf("x+y\n") is to literrally print what is between the quotes so %i is used for integers and the like
 
 }
 int add (int a, int b)//the first int in this line is the return value
