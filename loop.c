3cab2b6cff474e46e57d957a89c3f027eeab2dc5
Gimme-Wings <rgorelik2022@fau.edu>
2024-08-13 20:38:21 +0000

    Tue, Aug 13, 2024, 8:38 PM +00:00

--- loop.c
+++ loop.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+#include <cs50.h>
 
 int main (void)
 {
    int a = 0;
-  a get_int("what is a equal to?");
+   get_int ("what is a equal to?");
    while (a>2)
    {
     printf("meow\n");
