9c78d4f007f3914a95f15d18db59e2ff53c493fd
Gimme-Wings <rgorelik2022@fau.edu>
2024-08-14 18:35:25 +0000

    Wed, Aug 14, 2024, 2:35 PM -04:00

--- lp.c
+++ lp.c
@@ -4,8 +4,8 @@
 int main (void)
 {
 
-
-for (int l= get_int("what length? ");l>0;l--)
+int l= get_int("what length? ");
+for (int l ;l>0;l--)
 {
     for (int h= get_int("what height? ");h>0;h--)
 {
