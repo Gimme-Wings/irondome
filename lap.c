3feb83a1802d66750f7b5c1153eca6de913c6b97
Gimme-Wings <rgorelik2022@fau.edu>
2024-08-14 18:06:44 +0000

    Wed, Aug 14, 2024, 2:06 PM -04:00

--- lap.c
+++ lap.c
@@ -1,12 +1,16 @@
 #include <cs50.h>
 #include <stdio.h>
-
+void z (void);
 int main (void)
+
 {
 int x=4;
-int i=0;
-string z= (x-i)
+
+
 for (int i = 0 ;i < 4; i++)
+    z();
+}
+void z (void)
 {
-    printf("%c\n",z);
-} }
+    printf("#");
+}
