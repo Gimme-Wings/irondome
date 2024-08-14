f538609545e9285fbe58a66f5d79ff49775adc13
Gimme-Wings <rgorelik2022@fau.edu>
2024-08-14 20:00:32 +0000

    Wed, Aug 14, 2024, 4:00 PM -04:00

--- lp.c
+++ lp.c
@@ -8,7 +8,8 @@ print_row(h);
 }
  void print_row(int length)//function of print has an input of length that takes int as a input and it returns nothing because of void just performs the print
 {
-for (int i=0; i<length;i++)
+for (int i=0; i<h;i++)
+
 {
     printf("#");
 }
