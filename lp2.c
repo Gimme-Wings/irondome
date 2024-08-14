3972a463a1db0946816e2364a30f9493621ede86
Gimme-Wings <rgorelik2022@fau.edu>
2024-08-14 20:50:58 +0000

    Wed, Aug 14, 2024, 4:50 PM -04:00

--- lp2.c
+++ lp2.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <cs50.h>
 
-int main (void);
+int main (void)
 
 
-int h = get_int("height:");
-const int l = h;
+{int h = get_int("height:");
+int l = h;
 while (l>0)
 {
     printf("#");
     l--;
+}
 }
