f6086ad52eba21cf2341977754e0ee4c5ffb55a9
Gimme-Wings <rgorelik2022@fau.edu>
2024-08-14 16:29:06 +0000

    Wed, Aug 14, 2024, 12:29 PM -04:00

--- h2.c
+++ h2.c
@@ -6,6 +6,6 @@ int main (void)
 {
     string name = get_string ("what is your name?\n");
     int age = get_int ("what is your age\n");
-    int phone = get_int ("what is your phone number?\n");
-printf("Your info is %s, %i, %i", name, age, phone);
+    string phone = get_string ("what is your phone number?\n");
+printf("Your info is %s, %i, %s\n", name, age, phone);
 }
