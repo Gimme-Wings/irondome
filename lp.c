4b1f1a47e9df15baa4bbef8a00bd7af32fc4cdb4
Gimme-Wings <rgorelik2022@fau.edu>
2024-08-14 19:52:47 +0000

    Wed, Aug 14, 2024, 3:52 PM -04:00

--- lp.c
+++ lp.c
@@ -4,7 +4,7 @@ void print_row(int length);
 int main (void)
 {
 int h = get_int("what is the height?");
-print_row(4);
+print_row(h);
 }
  void print_row(int length)//function of print has an input of length that takes int as a input and it returns nothing because of void just performs the print
 {
