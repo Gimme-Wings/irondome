6b3059f8611301e3f336d57945a1bcfaad08d1cd
Gimme-Wings <rgorelik2022@fau.edu>
2024-08-15 02:01:55 +0000

    Wed, Aug 14, 2024, 10:01 PM -04:00

--- lp4.c
+++ lp4.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 #include <cs50.h>
-int row, column;
+int height, column;
 int main (void)
-
+{
 do
 {
-    row = get_int("height?\n");
-} while (row>8 || row <1);
+    height = get_int("height?\n");
+} while (height>8 || height <1);
+}
+{
+
+
+}
