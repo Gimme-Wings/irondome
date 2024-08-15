79b224fd93b3f5f33d8170f8901431db97fc462e
Gimme-Wings <rgorelik2022@fau.edu>
2024-08-15 02:07:47 +0000

    Wed, Aug 14, 2024, 10:07 PM -04:00

--- lp4.c
+++ lp4.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 #include <cs50.h>
-int height, column;
+int height, column,i;
 int main (void)
-{
+{{
 do
 {
     height = get_int("height?\n");
 } while (height>8 || height <1);
 }
 {
-
-
+for (i=0; i;height;i++)
+{
+    printf("_");
+}
+}
 }
