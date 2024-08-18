8ee2bb8adabd993fc21aa54f8a5c556a754a091b
Gimme-Wings <rgorelik2022@fau.edu>
2024-08-18 20:26:50 +0000

    Sun, Aug 18, 2024, 4:26 PM -04:00

--- cash.c
+++ cash.c
@@ -2,14 +2,13 @@
 #include <cs50.h>
 
 int main (void)
-{int change;
+{int change,leftover;
 do
 {
 change = get_int("how much change?");
 } while(change<0);
-if(change>25)
-{sub= 25
-
+if(change>0)
+{change= change-25;
 }
 
 
