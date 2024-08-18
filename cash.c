4ad587ffbe6c4bd4dcc834393334307a5e8b1ce2
Gimme-Wings <rgorelik2022@fau.edu>
2024-08-18 20:41:24 +0000

    Sun, Aug 18, 2024, 4:41 PM -04:00

--- cash.c
+++ cash.c
@@ -7,23 +7,18 @@ do
 {
 change = get_int("how much change?");
 } while(change<0);
-
-
 while (change>0)
 {
     if(change>0)
 {change= change-25;
 coins++;
 }
- if (change>0)
-{change= change-10;
+if(change>0)
+{
+    change= change-10;
     coins++;
-}if (change>0)
-{change= change-5;
-coins++;}
-if (change>0)
- {change= change-1;
-coins++;}
+}
+
 
 
 }printf("%i",coins);
