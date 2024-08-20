48cf411885845f5750ce54533de12a2062cb4176
Gimme-Wings <rgorelik2022@fau.edu>
2024-08-20 18:29:08 +0000

    Tue, Aug 20, 2024, 2:29 PM -04:00

---
+++ sarray.c
@@ -0,0 +1,12 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main (void)
+
+{
+    strings words[2];
+    words [0]= "hi";
+    words [1]= "bye";
+    printf("%s\n",words[0]);
+    printf("%s\n", words[1]);
+}
