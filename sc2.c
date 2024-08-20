0aa73a927a20d54f0e546c994bfc29e1c3f1ec20
Gimme-Wings <rgorelik2022@fau.edu>
2024-08-20 17:58:38 +0000

    Tue, Aug 20, 2024, 1:58 PM -04:00

--- sc2.c
+++ sc2.c
@@ -4,9 +4,9 @@
 
  {
     int scores[3];
-    for (i=0; i<3;i++)
+    for (int i=0; i<3;i++)
     {
-        
+        scores[i]=get_int("score: ");
     }
     printf("Average: %f\n",(scores[0]+scores[1]+scores[2])/3.0);
 }
