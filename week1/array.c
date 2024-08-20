bef5f5412964ed107f26ea07993b8d9253c278e8
Gimme-Wings <rgorelik2022@fau.edu>
2024-08-20 17:51:21 +0000

    Tue, Aug 20, 2024, 1:51 PM -04:00

---
+++ week1/array.c
@@ -0,0 +1,13 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main (void)
+
+{
+    int scores[3];
+    scores [0]= get_int ("score: ");
+    scores [1]= get_int ("score: ");
+    scores [2]= get_int ("score: ");
+
+    printf("Average: %f\n",(scores[0],scores[1],scores[2])/3.0)
+}
