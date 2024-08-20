12024f0a276d2ad15ab485a5376f76ae7c1df51f
Gimme-Wings <rgorelik2022@fau.edu>
2024-08-20 18:03:55 +0000

    Tue, Aug 20, 2024, 2:03 PM -04:00

--- sc2.c
+++ sc2.c
@@ -2,12 +2,12 @@
 #include <stdio.h>
  int main(void)
 
- { int N=3;
-    int scores[N];
+ { const int N=3;//const ensures that the number doesnt change
+    int scores[N];/put in N to avoid floating around numbers
     for (int i=0; i<N;i++)
     {
-        scores[i]=get_int("score: ");
+        scores[i]=get_int("score: ");// do it however many times is i
     }
-    printf("Average: %f\n",(scores[1]+scores[2]+scores[3])/(float)N);
+    printf("Average: %f\n",(scores[1]+scores[2]+scores[3])/(float)N);//put (float) in front of number to turn it into a float
 }
 
