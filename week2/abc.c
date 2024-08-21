b530aae6c2cfad36b7b3d004ccb54a3761706ddb
Gimme-Wings <rgorelik2022@fau.edu>
2024-08-21 15:07:21 +0000

    Wed, Aug 21, 2024, 11:07 AM -04:00

--- week2/abc.c
+++ week2/abc.c
@@ -6,8 +6,10 @@ int main (void)
     string phrase= get_string ("enter phrase: ");
 int length = strlen (phrase);
 for (int i = 0; i < length; i++)
+{if (phrase[i]>phrase[i+1])
 {
-printf("%i ",phrase [i]);
+    
+}
 
 } printf("\n");
 }
