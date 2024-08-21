3707e5f35acc523863fdea8a3f4cf2a2c0785d9e
Gimme-Wings <rgorelik2022@fau.edu>
2024-08-21 15:22:44 +0000

    Wed, Aug 21, 2024, 11:22 AM -04:00

--- week2/abc.c
+++ week2/abc.c
@@ -1,17 +1,21 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <string.h>
-int main (void)
+int main(void)
 {
-    string phrase= get_string ("enter phrase: ");
-int length = strlen (phrase);
-for (int i = 0; i < length-1; i++)
-{if (phrase[i]>phrase[i+1])
-{
-    printf("not\n");
-    return 0;
+    string phrase = get_string("enter phrase: ");
+    int length = strlen(phrase);
+    for (int i = 0; i < length - 1; i++)
+    {
+        if (phrase[i] > phrase[i + 1])
+        {
+            printf("not\n");
+            return 0;
+        }
+        else
+        {
+            printf("alphabetical\n");
+        }
+    }
+    printf("\n");
 }
-
-} printf("\n");
-}
-
