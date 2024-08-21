cb0f0945174c6dfa59dd363d97d963aa4b014edb
Gimme-Wings <rgorelik2022@fau.edu>
2024-08-21 14:45:56 +0000

    Wed, Aug 21, 2024, 10:45 AM -04:00

--- week2/double.c
+++ week2/double.c
@@ -3,16 +3,16 @@
 
 int main (void)
 
-{int size =5;
+{int size =get_int ("size: ");
     int sequence [size];
     sequence [0]=1;
 printf("%i\n", sequence [0]);
-    for ( int i =1; i < 5; i++)
+    for ( int i =1; i < size; i++)
 
     {
         sequence[i]= sequence [i-1]*2;
-
-    }printf("%i\n", sequence [i]);
+printf("%i\n", sequence [i]);
+    }
 
 
 }
