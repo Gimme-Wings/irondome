521ff66b351f39810b4fdc3ee0e2c84d931e37d1
Gimme-Wings <rgorelik2022@fau.edu>
2024-08-21 15:42:37 +0000

    Wed, Aug 21, 2024, 11:42 AM -04:00

--- week2/mario.c
+++ week2/mario.c
@@ -4,6 +4,11 @@
 
 int main (int argc, string argv [])
 {
+    if (argc !=2)
+{
+    printf("usage: ./mario number\n");
+    return 1;
+}
     int height =atoi(argv[1]); //to store a number that will be input at the command line;
 }
 // as an int, the stdlib.h has to be included and use atoi to change it; from a string to an int;
