658811a682ad1eeae4e6209f82b71ec165ea045c
Gimme-Wings <rgorelik2022@fau.edu>
2024-08-21 14:51:25 +0000

    Wed, Aug 21, 2024, 10:51 AM -04:00

---
+++ week2/abc.c
@@ -0,0 +1,13 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+int main (void)
+{
+    string phrase= get_string ("enter phrase: ");
+    int length = strlen(phrase);
+for (int i=0; i<length;i++)
+{
+     
+}
+}
+
