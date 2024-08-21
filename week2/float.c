5dd57ed99d320c2b86c2b5c229350f08928874bc
Gimme-Wings <rgorelik2022@fau.edu>
2024-08-21 18:59:59 +0000

    Wed, Aug 21, 2024, 2:59 PM -04:00

--- week2/float.c
+++ week2/float.c
@@ -1,14 +1,24 @@
 #include <cs50.h>
 #include <stdio.h>
-float multiply (float a, float b);
 
-int main (void)
-{{ float a = get_float("a: \n");
-float b = get_float("b: \n");
-multiply(float a, float b);
-printf("%f\n", multiply);
-}
-float multiply (float a, float b);
+// Function prototype
+float multiply(float a, float b);
+
+int main(void)
 {
-    return x*y;
+    // Get input from the user
+    float a = get_float("a: ");
+    float b = get_float("b: ");
+
+    // Call the multiply function and store the result
+    float result = multiply(a, b);
+
+    // Print the result
+    printf("%f\n", result);
+}
+
+// Function definition
+float multiply(float a, float b)
+{
+    return a * b;
 }
