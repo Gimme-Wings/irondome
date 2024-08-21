e0bea28b47c3488e36ffa4b98eb8fa8ec65cf52e
Gimme-Wings <rgorelik2022@fau.edu>
2024-08-21 18:51:51 +0000

    Wed, Aug 21, 2024, 2:51 PM -04:00

--- week2/float.c
+++ week2/float.c
@@ -1,15 +1,14 @@
 #include <cs50.h>
 #include <stdio.h>
-float mult (float a, float b);
+float multiply (float a, float b);
 
 int main (void)
 {{ float a = get_float("a: \n");
-
 float b = get_float("b: \n");
 mult(c);
 printf("%f\n", c);
 }
-float mult (float a, float b);
+float multiply (float a, float b);
 {float c= a*b;
  c=  return a*b;
 }
