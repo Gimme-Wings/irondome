6bd2a36fcddbf86a61e25eec1f13ab9f677f300f
Gimme-Wings <rgorelik2022@fau.edu>
2024-08-21 15:36:41 +0000

    Wed, Aug 21, 2024, 11:36 AM -04:00

---
+++ week2/mario.c
@@ -0,0 +1,9 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (int argc, string argv [])
+{
+    int height =atoi(argv[1]) //to store a number that will be input at the command line as an int, the stdlib.h has to be included and use atoi to change it; from a string to an int;
+}
+//the stdlib.h has to be included and use atoi to change it; from a string to an int;
