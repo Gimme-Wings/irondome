f4f04aec3a5d0404be46e9c8bdbdfb7d49a55f66
Gimme-Wings <rgorelik2022@fau.edu>
2024-08-22 19:52:22 +0000

    Thu, Aug 22, 2024, 3:52 PM -04:00

--- week2/tri.c
+++ week2/tri.c
@@ -3,15 +3,16 @@
 
 bool tri (int a, int b, int c);
 int main (void)
-{
-    {int a,b,c, value=0;
-    {
+
+    {  int a,b,c, value=0;
+
 do
 { a = get_int("a:");
  b = get_int ("b:");
  c = get_int ("c:");
+
     } while ( a <= 0 ||  b <= 0 || c <=0);
-}
+
 if (value==0)
 {
     printf("true\n");
@@ -20,13 +21,13 @@ else
 {
     printf("false\n");
 }
-}
+
 
 {
     bool tri (int a, int b, int c)
-{ return (a+b>c && a+c>b && b+c>a)
+{ return (a+b>c) && (a+c>b) && (b+c>a);
 
 value=0
 }
 }
-}
+
