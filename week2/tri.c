8219e90902132f231646240884108ae732e31d9d
Gimme-Wings <rgorelik2022@fau.edu>
2024-08-22 19:36:56 +0000

    Thu, Aug 22, 2024, 3:36 PM -04:00

--- week2/tri.c
+++ week2/tri.c
@@ -1,6 +1,7 @@
 #include <cs50.h>
 #include <stdio.h>
 
+char tri (int a, int b, int c);
 int main (void)
 {int a,b,c;
     {
@@ -9,7 +10,18 @@ do
  b = get_int ("b:");
  c = get_int ("c:");
     } while ( a <= 0 ||  b <= 0 || c <=0);
+}if (char tri (int a, int b, int c)=0)
+{
+    printf("true\n");
+}
+else
+{
+    printf("false\n");
+}
 }
 
-}
+{char tri (int a, int b, int c)
+{if ()
 
+}
+}
