589c16a273567412f3251c8868ff78806fe42a03
Gimme-Wings <rgorelik2022@fau.edu>
2024-08-22 19:25:16 +0000

    Thu, Aug 22, 2024, 3:25 PM -04:00

---
+++ week2/tri.c
@@ -0,0 +1,15 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main (void)
+{
+do
+{
+    while (int a<0 || int b<0 ||int c <0)
+    {
+int a = get_int("a:");
+int b = get_int ("b:");
+int c = get_int ("c:");
+    }
+}
+}
