96a1c7d9cfb0aa806563f94ecdaf9980f4950481
Gimme-Wings <rgorelik2022@fau.edu>
2024-08-22 19:28:42 +0000

    Thu, Aug 22, 2024, 3:28 PM -04:00

--- week2/tri.c
+++ week2/tri.c
@@ -2,14 +2,13 @@
 #include <stdio.h>
 
 int main (void)
-{
+{{
 do
-{
-    while (int a<0 || int b<0 ||int c <0)
-    {
-int a = get_int("a:");
+{int a = get_int("a:");
 int b = get_int ("b:");
 int c = get_int ("c:");
-    }
+    } while (int a < 0 || int  b < 0 || int c <0)
 }
+
 }
+
