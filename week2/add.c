09a838b9ebb8c8575f9a0caedeba19c65e6b117b
Gimme-Wings <rgorelik2022@fau.edu>
2024-08-22 12:19:05 +0000

    Thu, Aug 22, 2024, 8:19 AM -04:00

--- week2/add.c
+++ week2/add.c
@@ -1,10 +1,20 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int add_two(int a, int b)
+int add_two(int a, int b);
 int main (void)
 
 {
-    int a = get_int("a:\n ");
-    int b = get_int("b:\n ");
+    int a = get_int("a: ");
+    int b = get_int("b: ");
+
+    int result= add_two (a,b);
+
+    printf("a*b=%i\n", result);
+
+}
+
+int add_two(int a, int b)
+{
+    return = a + b;
 }
