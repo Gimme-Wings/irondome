0fc1ffe4a98a806a3133b0082596164921fb1720
Gimme-Wings <rgorelik2022@fau.edu>
2024-08-23 01:41:52 +0000

    Thu, Aug 22, 2024, 9:41 PM -04:00

--- week2/scrabble/scrabble.c
+++ week2/scrabble/scrabble.c
@@ -6,13 +6,13 @@ int main (void)
 string a = get_string ("Player 1: ");
 string b = get_string ("Player 2: ");
 
-int value1 [a]={}
+int value1[a]
 }
 
 int sum1 = 0;
     for (int i = 0; i < length; i++)
     {
-        sum1 += array[i];
+        sum1 += value1[i];
     }
     return sum1;
 }
