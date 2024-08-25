42586b665487323f587535f65db010ae4f58e11d
Gimme-Wings <rgorelik2022@fau.edu>
2024-08-25 20:01:36 +0000

    Sun, Aug 25, 2024, 4:01 PM -04:00

--- week2/scrabble/scrabble.c
+++ week2/scrabble/scrabble.c
@@ -3,7 +3,8 @@
 
 int main (void)
 {//get a and b strings
-string a = get_string ("Player 1: ");
-string b = get_string ("Player 2: ");
-printf("%i%i\n", a,a)
+string words[2];
+words[0] = get_string ("Player 1: ");
+words[1] = get_string ("Player 2: ");
+printf("%i%i\n", words[0][0], words[1][0]);
 }
