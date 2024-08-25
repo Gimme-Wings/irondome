f6f3ca76fcdf43526dabc776ab2fe344bed5e47e
Gimme-Wings <rgorelik2022@fau.edu>
2024-08-25 20:08:28 +0000

    Sun, Aug 25, 2024, 4:08 PM -04:00

--- week2/scrabble/scrabble.c
+++ week2/scrabble/scrabble.c
@@ -1,10 +1,39 @@
 #include <cs50.h>
 #include <stdio.h>
-
+int add ()
 int main (void)
 {//get a and b strings
 string words[2];
 words[0] = get_string ("Player 1: ");
 words[1] = get_string ("Player 2: ");
-printf("%i%i\n", words[0][0], words[1][0]);
+
+
+if (sum1>sum2)
+{
+    printf("player 1 wins\n");
+}
+if else (sum2> sum1)}
+{
+    printf("player 2 wins\n");
+}
+else
+{
+    printf("tie\n");
+}
+
+
+{
+int sum1 = 0;
+    for (int i = 0; i < length; i++)
+    {
+        sum1 += words1[i];
+    }
+    return sum1;
+}
+int sum2 = 0;
+    for (int k = 0; k < length; k++)
+    {
+        sum2 += words2[i];
+    }
+    return sum2;
 }
