527fa0a9a56d905c7db5f4da091516742127449b
Gimme-Wings <rgorelik2022@fau.edu>
2024-08-28 23:58:49 +0000

    Wed, Aug 28, 2024, 7:58 PM -04:00

---
+++ yeshivish.c
@@ -0,0 +1,43 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main (void)
+
+{
+    printf("type y or n to indicate answer");
+char a = get_char("do they wear a white shirt? (y/n)");
+
+
+
+if(a==y)
+{
+    printf("maybe they are still normal");
+}
+else
+{
+    printf("going OTD");
+    return 0;
+}
+char b = get_char("is it CT?");
+
+if (b==y)
+{
+    printf("closer to special");
+}
+else
+{
+    printf("probably just regular yeshivish");
+    return 0;
+}
+char c = get_char("do they make weird noises?")
+if (c==y)
+{
+    printf("probably a brisker");
+}
+else
+{
+    printf("A CHOFETZ CHAIMER");
+    return 0;
+}
+
+}
