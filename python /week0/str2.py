b980a87f7ff7d7d8f48530d49e2d38ed2e22ac57
Gimme-Wings <rgorelik2022@fau.edu>
2024-08-28 22:03:49 +0000

    Wed, Aug 28, 2024, 6:03 PM -04:00

--- python /week0/str2.py
+++ python /week0/str2.py
@@ -1,4 +1,5 @@
 #.lstrip() removbes from left rstrip removes from right
+#title captilizes
 name = input ("whats your name? ").strip().title()
 #or remove from line above and do name = name.strip()
 #splits name into two parts
