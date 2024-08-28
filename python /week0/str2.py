ce4b8283d38fb61b4cd8b837121b50d2d31c4027
Gimme-Wings <rgorelik2022@fau.edu>
2024-08-28 22:03:11 +0000

    Wed, Aug 28, 2024, 6:03 PM -04:00

--- python /week0/str2.py
+++ python /week0/str2.py
@@ -1,7 +1,8 @@
 #.lstrip() removbes from left rstrip removes from right
 name = input ("whats your name? ").strip().title()
 #or remove from line above and do name = name.strip()
+#splits name into two parts
 first, last= name.split(" ")
 
 
-print(f"hello, {name}")
+print(f"hello, {first}")
