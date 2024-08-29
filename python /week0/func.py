179107743abcf093c97f6ed024732795f5bd1b2f
Gimme-Wings <rgorelik2022@fau.edu>
2024-08-29 00:31:35 +0000

    Wed, Aug 28, 2024, 8:31 PM -04:00

--- python /week0/func.py
+++ python /week0/func.py
@@ -1,6 +1,6 @@
-def hello():#()  is saying taking no input whatever is inside is a parameter
-    print("hello,", to)
+def hello(name):#()  is saying taking no input whatever is inside is a parameter and when the function is called what is inside() below will be copied above into the () and the computer will asumme its the same
+    print("hello,", name)#if taking input the name of parameter has to go in () also below
 
 name = input("whats your name? ")
-hello()
-print(name)
+hello(name)
+
