91c772c5f6f8711b3ae45bf503325e44419a0050
Gimme-Wings <rgorelik2022@fau.edu>
2024-08-29 20:26:50 +0000

    Thu, Aug 29, 2024, 4:26 PM -04:00

--- python /input.py
+++ python /input.py
@@ -2,9 +2,9 @@ def greet (input):
     if "hello" in input:
         return ("Whats going on, ")#or print it
     else:
-        return ("IDK")
+        return 0
 
-def beg():
+def main():
     ask =  input("if you want to start, type hello: ")
                               #store is storing the value return through greet/input it can be accesed later
     store = greet(ask) #this is the input thats referenced above
@@ -12,6 +12,7 @@ def beg():
         name = input("whats your name? ")
         name= name.title()
 #can use stored value to add to it
-print("Hey, " + store + name)
+    print("Hey, " + store + name)
 
-beg()
+
+main()
