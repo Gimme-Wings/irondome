41c4f74098a165c09ca741603af43426ed1171b9
Gimme-Wings <rgorelik2022@fau.edu>
2024-08-29 00:53:58 +0000

    Wed, Aug 28, 2024, 8:53 PM -04:00

--- python /week0/square.py
+++ python /week0/square.py
@@ -3,6 +3,6 @@ def main ():
     print("x squared is", square (x))
 
 def square(n):
-    return n*n
+    return n*n #to square can also do n ** 2 or pow(n,3) 3 being the exponent in this case
 
 main()
