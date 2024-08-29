1b9121f7ac45090369a923e88e219c95677d733c
Gimme-Wings <rgorelik2022@fau.edu>
2024-08-29 23:45:18 +0000

    Thu, Aug 29, 2024, 7:45 PM -04:00

--- playback/playback.py
+++ playback/playback.py
@@ -1,3 +1,6 @@
-fast = input("type something: ")
-fast= fast.replace(" ","...")
-print(fast)
+def fast():
+    fast = input("type something: ")
+    fast= fast.replace(" ","...")
+    print(fast)
+
+fast()
