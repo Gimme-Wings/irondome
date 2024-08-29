ed04072be3fa94847800e455d5d3d1166cd791a9
Gimme-Wings <rgorelik2022@fau.edu>
2024-08-29 22:40:16 +0000

    Thu, Aug 29, 2024, 6:40 PM -04:00

--- python /week0/machine.py
+++ python /week0/machine.py
@@ -1,10 +1,11 @@
 emoticon = "v.v"
 emoticon2 = "<0> <0>"
 
-
+#execute main/change global variable to modifiable instead of accesbile
 def main():
+    global emoticon
     say("is anyone there? ")
-    emoticon = ":D"
+    emoticon = ":D"#python doesnt want to change a global variable
     say("hello")
 
 def say(phrase):
