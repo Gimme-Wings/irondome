4dc7159c5c7b28fbc37652fe05b6c031188c93c3
Gimme-Wings <rgorelik2022@fau.edu>
2024-08-29 22:04:48 +0000

    Thu, Aug 29, 2024, 6:04 PM -04:00

--- python /week0/machine.py
+++ python /week0/machine.py
@@ -1,12 +1,14 @@
 emoticon = "v.v"
+emoticon2 = "<0> <0>"
 
 
 def main():
     say("is anyone there? ")
-
+    say("hello")
 
 def say(phrase):
     print(phrase+ " " + emoticon)
+    print(phrase+ " " + emoticon2)
 
 
 main()
