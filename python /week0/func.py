df1740192075aee94a6c14c7240b917a758ac510
Gimme-Wings <rgorelik2022@fau.edu>
2024-08-29 00:28:06 +0000

    Wed, Aug 28, 2024, 8:28 PM -04:00

--- python /week0/func.py
+++ python /week0/func.py
@@ -1,5 +1,5 @@
-def hello():#()  is saying taking no input
-    print("hello")
+def hello():#()  is saying taking no input whatever is inside is a parameter
+    print("hello,", to)
 
 name = input("whats your name? ")
 hello()
