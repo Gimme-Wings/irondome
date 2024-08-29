b2e01adeb3b56316e397e02271afbf5c1ef63d91
Gimme-Wings <rgorelik2022@fau.edu>
2024-08-29 21:49:45 +0000

    Thu, Aug 29, 2024, 5:49 PM -04:00

--- python /week0/guess.py
+++ python /week0/guess.py
@@ -1,10 +1,10 @@
 def get_guess():
-    guess=input("guess: ")
+    guess=int(input("guess: "))
     return guess
 
 def main():
     guess= get_guess()
-    if guess == 50:
+    if guess == 50: # if checking a number ask for int above
         print("correct")
     else:
         print("incorrect")
