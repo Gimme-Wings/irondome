25a120b06e2161d5a9d2be6c67a715887cf1614b
Gimme-Wings <rgorelik2022@fau.edu>
2024-08-29 20:16:00 +0000

    Thu, Aug 29, 2024, 4:16 PM -04:00

--- python /input.py
+++ python /input.py
@@ -1,11 +1,12 @@
 def greet (input):
     if "hello" in input:
-        return ("whats going on")#or print it
+        return ("whats going on ")#or print it
     else:
         return ("IDK")
 #store is storing the value return through greet/input it can be accesed later
 store = greet("hello") #this is the input thats referenced above
-if store= 'whats going on'
-    input=("whats your name ")
+if store == ('whats going on '):
+    name = input("whats your name? ")
+    name= name.title
 #can use stored value to add to it
-print("hey" + store)
+print("hey " + store + name)
