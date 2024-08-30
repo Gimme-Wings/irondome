55d7810aa7b430fb28a7be8b370b2bf371eb5b9c
Gimme-Wings <rgorelik2022@fau.edu>
2024-08-30 00:10:40 +0000

    Thu, Aug 29, 2024, 8:10 PM -04:00

--- faces/faces.py
+++ faces/faces.py
@@ -2,9 +2,11 @@ def convert(text):
     if ":)" in text:
         text = text.replace(":)","🙂")
         print(text)
+        return 0
     if ":(" in text:
         text = text.replace(":(","🙁")
         print(text)
+        return 0
 
 def main ():
     emoji = input("input: ")
