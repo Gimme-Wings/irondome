86fc06bc24440421cc62e2e11534f1d6f8bf460e
Gimme-Wings <rgorelik2022@fau.edu>
2024-09-01 22:21:19 +0000

    Sun, Sep 1, 2024, 6:21 PM -04:00

--- python /week1/house.py
+++ python /week1/house.py
@@ -1,9 +1,18 @@
 name  = input("whats your name? ")
 
-if name == "Harry" or name == "hermione" or name == "ron" :
-    print("G")
+#if name == "Harry" or name == "hermione" or name == "ron" :
+#    print("G")
 
-elif name == "draco":
-    print("s")
-else:
-    print("who")
+#elif name == "draco":
+ #   print("s")
+#else:
+ #   print("who")
+
+
+match name:
+    case "harry" | "ron" | "hermione":# these are or statements
+        print("G")
+    case "Draco":
+        print("S")
+    case _:#for cases not mentioned before
+        print("who")
