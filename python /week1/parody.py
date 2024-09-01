0c08519ec6f6b9e8f609bf4a1e3bfa813ad52821
Gimme-Wings <rgorelik2022@fau.edu>
2024-09-01 21:46:19 +0000

    Sun, Sep 1, 2024, 5:46 PM -04:00

---
+++ python /week1/parody.py
@@ -0,0 +1,11 @@
+#check if even or odd by %
+#get number
+x = int(input("which number? "))
+#check if divisible by 2 fold all into z
+z = x%2
+#check if remainder is 0
+if z == 0:
+    print("even")
+else :
+    print("odd")
+
