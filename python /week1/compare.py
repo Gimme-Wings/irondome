0ee7861ced7138f40eaa2f60035c6af23b920bf1
Gimme-Wings <rgorelik2022@fau.edu>
2024-09-01 20:47:34 +0000

    Sun, Sep 1, 2024, 4:47 PM -04:00

--- python /week1/compare.py
+++ python /week1/compare.py
@@ -2,8 +2,11 @@ x = int(input("Whats x? "))
 y= int(input("whats y? "))
 
 #boolean expression (true or false)
-if x<y:
+if x < y:# only execute indented line if above is true
     print("x is less than y")
-
+if x > y:
+    print("x is greater than y")
+if x == y:
+    print("x is equal to y")
 
 
