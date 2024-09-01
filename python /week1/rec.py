029c7fd3d8a7d2d0fd5274ed5971de2a2fb384be
Gimme-Wings <rgorelik2022@fau.edu>
2024-09-01 23:06:31 +0000

    Sun, Sep 1, 2024, 7:06 PM -04:00

--- python /week1/rec.py
+++ python /week1/rec.py
@@ -1,26 +1,23 @@
 def main():
     difficulty = input("Difficult or Casual? ")
-    if not(difficulty == "Difficult" or == "casual") #not negates the ()
+    if not(difficulty == "Difficult" or difficulty == "casual"): #not negates the ()
      print("Enter a valid difficulty")
      return
     players = input("Multiplayer or Single-player? ")
-    if not(players == "Multiplayer" or == "single-player")
+    if not(players == "Multiplayer" or players == "single-player"):
         print("Enter a valid number of players")
         return
 
     if difficulty == "Difficult" and players == "Multiplayer":
             recommend("Poker")
-        elif players == "Single-player":
+    elif difficulty == "difficult" and players == "Single-player":
             recommend("Klondike")
-        else:
-            print("Enter a valid number of players")
-    elif difficulty == "Casual":
-        if players == "Multiplayer":
+
+    elif difficulty == "Casual" and players == "Multiplayer":
             recommend("Hearts")
-        elif players == "Single-player":
+    elif difficulty == "Casual" and players == "Single-player":
             recommend("Clock")
-        else:
-            print("Enter a valid number of players")
+
 
 
 
