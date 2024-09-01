eec86ece4052d8d28ecee1b26ffe44e47f4dadf2
Gimme-Wings <rgorelik2022@fau.edu>
2024-09-01 20:53:58 +0000

    Sun, Sep 1, 2024, 4:53 PM -04:00

--- python /week1/compare.py
+++ python /week1/compare.py
@@ -6,7 +6,7 @@ if x < y:# only execute indented line if above is true
     print("x is less than y")
 elif x > y:
     print("x is greater than y")
-else:
+elif x == y:
     print("x is equal to y")
 
 
