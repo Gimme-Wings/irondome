02241c51557a28b5900f7cfd59e9934bfc36af7f
Gimme-Wings <rgorelik2022@fau.edu>
2024-09-04 01:07:31 +0000

    Tue, Sep 3, 2024, 9:07 PM -04:00

--- interpreter/interpreter.py
+++ interpreter/interpreter.py
@@ -8,13 +8,13 @@ y = (numbers[2])
 z = float(numbers[4])
 
 if "+" in y:
-    print(x + y)
+    print(x + z)
 elif "-" in y:
-    print(x - y)
+    print(x - z)
 elif "*" in y:
-    print(x * y)
+    print(x * z)
 elif "/" in y:
-    if y != 0:
-        print(x / y)
+    if z != 0:
+        print(x / z)
     else:
         print("undefined")
