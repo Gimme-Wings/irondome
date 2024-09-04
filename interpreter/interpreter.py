9bcafd817c05ad19246c62a811db5dfc2f8f0ceb
Gimme-Wings <rgorelik2022@fau.edu>
2024-09-04 01:00:46 +0000

    Tue, Sep 3, 2024, 9:00 PM -04:00

--- interpreter/interpreter.py
+++ interpreter/interpreter.py
@@ -1,4 +1,14 @@
 numbers = input("Expression: ")
 
-x = numbers[2]
+
+x = float(numbers[0])
 print(x)
+
+y = (numbers[2])
+
+z = float(numbers[4])
+
+if y == "+":
+    print(x + y)
+elif y == "-":
+    print(x - y)
