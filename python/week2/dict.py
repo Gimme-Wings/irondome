5577099e261e39d6f8021c0897c91e9945f756cc
Gimme-Wings <rgorelik2022@fau.edu>
2024-09-05 17:16:22 +0000

    Thu, Sep 5, 2024, 1:16 PM -04:00

--- python/week2/dict.py
+++ python/week2/dict.py
@@ -5,9 +5,11 @@ students = {
     "ron":"gryf",
     "draco":"slyth",
 }
-name = input("what name? ")
-print(students[name])#can use words to index
+#name = input("what name? ")
+#print(students[name])    #can use words to index
 # or
 
-for student in students:
-    print(student)
+#for student in students:     #this iterates only the keys(names of kids)
+   # print(student)
+for i in students:
+    print(i,students[i], sep=", ") #first prints the i which is the key and then it calls the dictionary by i position
