50e51429151d0d5323bcf285a5173986bc6c5552
Gimme-Wings <rgorelik2022@fau.edu>
2024-09-05 18:52:42 +0000

    Thu, Sep 5, 2024, 2:52 PM -04:00

--- python/week2/mario.py
+++ python/week2/mario.py
@@ -1,12 +1,15 @@
 def main():
-   while True: #induce a forvever loop
-    height = int(input("what height? "))
-    if height <= 0:
-        continue
-    else:
-        return height
-    
-    for _ in range(height):
-        print("#")
+   x = ask_height()
+   print_column(x)
+
+
+def print_column(height):
+   for _ in range(height):
+      print("#")
+
+def ask_height():
+   ask = int(input("whats the height? "))
+   return ask
+
 
 main()
