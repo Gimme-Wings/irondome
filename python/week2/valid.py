5a17c17b9b5d4052c7337613cc34f0ff02feb61f
Gimme-Wings <rgorelik2022@fau.edu>
2024-09-05 15:46:31 +0000

    Thu, Sep 5, 2024, 11:46 AM -04:00

--- python/week2/valid.py
+++ python/week2/valid.py
@@ -1,3 +1,8 @@
-n = int(input("whats n? "))
-while n != int:
-    print("what")
+while True: #induce a forvever loop
+    n = int(input("whats n? "))
+    if n < 0:
+        continue
+    else:
+        break
+#if true ask for input then check if its less than 0, if its less than 0 continue th functione
+# if its greater or equal to 0 go to else and break the most recent loop entered
