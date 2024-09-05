2be3a5681f217d6446b001538e6d2da4b2ecf4e5
Gimme-Wings <rgorelik2022@fau.edu>
2024-09-05 02:27:36 +0000

    Wed, Sep 4, 2024, 10:27 PM -04:00

--- meal/meal.py
+++ meal/meal.py
@@ -1,10 +1,14 @@
 def main():
-    ...
+    time = input("What time is it? ")
+    convert(time)
 
 
 def convert(time):
-    ...
+    type = time.find("")
 
 
 if __name__ == "__main__":
     main()
+
+
+#7:00 and 8:00, lunch between 12:00 and 13:00, and dinner between 18:00 and 19:00.
