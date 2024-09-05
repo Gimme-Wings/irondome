e815342b55a773d0cc4f7b9efe7a67053d9509bb
Gimme-Wings <rgorelik2022@fau.edu>
2024-09-05 16:23:31 +0000

    Thu, Sep 5, 2024, 12:23 PM -04:00

--- python/week2/hogwarts.py
+++ python/week2/hogwarts.py
@@ -2,6 +2,13 @@ students = ["Hermione", "Harry", "Ron"]# [] is a list
 
 print(students[0:3])#index into the list
 
+print("")
+
 print(students[0])
 print(students[1])
 print(students[2])
+# or better use a loop
+print("")
+
+for s in students: #s is a made up variable and just checks how many times to do the action
+    print(s)
