e3528c90238ff1025dd275f5ee50dcd6756f9819
Gimme-Wings <rgorelik2022@fau.edu>
2024-09-05 23:36:48 +0000

    Thu, Sep 5, 2024, 7:36 PM -04:00

--- python/week2/for2.py
+++ python/week2/for2.py
@@ -3,4 +3,17 @@ def main():
 
     for i in range(len(names)): # i is being iterated
         print(i)
+
+    for i in range(len(names)):
+        print( write_letter(names[i], "princess"))
+
+def write_letter(receiver, sender):
+    return f""" Dear {receiver},
+come to the party.
+sincerely,
+{sender}
+
+"""
+
+
 main()
