b8ab30161e439edbe40967b5a62fef3c4d60690f
Gimme-Wings <rgorelik2022@fau.edu>
2024-09-05 15:17:18 +0000

    Thu, Sep 5, 2024, 11:17 AM -04:00

--- python/week2/for.py
+++ python/week2/for.py
@@ -8,5 +8,6 @@ for i in range(3):
 for _ in range(3): # if not using the variable at all just call it _
     print("woof")
 
-print("dog\n"  *3)
+
+print("dog\n"  *3, end = "")# end=## is so that it doesnt enter one more time
 
