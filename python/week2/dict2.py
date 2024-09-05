0844c75d49cc1ab938048f54e1b1d1271d413a41
Gimme-Wings <rgorelik2022@fau.edu>
2024-09-05 18:10:18 +0000

    Thu, Sep 5, 2024, 2:10 PM -04:00

---
+++ python/week2/dict2.py
@@ -0,0 +1,7 @@
+#using multiple dict
+students = [
+    {"name": "Hermione", "house": "Gryffindor", "patronus": "otter"},
+    {"name": "Harry", "house": "Gryffindor", "patronus": "stag"},
+    {"name": "ron", "house": "gryffindor", "patronus": "jack russel terrier"},
+    {"name": "Draco", "house": "slytherin", "patronus": None}
+]
