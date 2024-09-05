19570238037fb49aba5cd02a31e4a6f27031b705
Gimme-Wings <rgorelik2022@fau.edu>
2024-09-05 16:50:02 +0000

    Thu, Sep 5, 2024, 12:50 PM -04:00

--- python/week2/len.py
+++ python/week2/len.py
@@ -1,4 +1,4 @@
 students = ["hermione", "harry", "ron"]
 
 for i in range(len(students)):#cant put students into () becasuse students is not a number and range needs a number
-    print(students[i])#when i is 0 it prints 0th place in the index
+    print(i, students[i])#when i is 0 it prints 0th place in the index
