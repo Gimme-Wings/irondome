5f1a5c7fd3be1134dc8ec044854b4a7fc1ee8994
Gimme-Wings <rgorelik2022@fau.edu>
2024-09-09 11:46:59 +0000

    Mon, Sep 9, 2024, 7:46 AM -04:00

--- camel/camel.py
+++ camel/camel.py
@@ -1,10 +1,12 @@
 
 name = input("camel case: ")
-
+print("snake case: ", end = "")
 for c in name:
+
     if c.islower:
         print(c, end = "")
     elif c.isupper:
-        c = c.lower(c)
+        c.lower()
         print("_")
         print(c, end = "")
+
