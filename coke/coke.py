f4f1ae9e6d11ca7ebb655e35e5dc4b6ad2da1161
Gimme-Wings <rgorelik2022@fau.edu>
2024-09-10 11:19:07 +0000

    Tue, Sep 10, 2024, 7:19 AM -04:00

--- coke/coke.py
+++ coke/coke.py
@@ -8,10 +8,13 @@ while cost > 0:
         if (n != 25) and (n!= 10) and (n!=5):
             continue
         else:
-            if (cost-n)<0:
-                print(f"Change Owed: {n-cost}")
-            if( cost-n)>0:
-                cost = cost - n
-                print(f"Amount Due: {cost}")
+            cost = cost - n
+            print(f"Amount Due: {cost}")
+        if (cost-n)<0:
+            print(f"Change Owed: {n-cost}")
+
+            #if( cost-n)>0:
+                #cost = cost - n
+                #print(f"Amount Due: {cost}")
             break
 
