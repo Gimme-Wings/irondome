faccc3c231d071d2489fe35044463d7ee77fab4a
Gimme-Wings <rgorelik2022@fau.edu>
2024-09-10 11:06:34 +0000

    Tue, Sep 10, 2024, 7:06 AM -04:00

--- coke/coke.py
+++ coke/coke.py
@@ -5,7 +5,7 @@ while cost > 0:
     while True: #induce a forvever loop
         print(f"Amount Due: {cost}")
         n = int(input("Insert coin: "))
-        if (n != 25) and (n!= 10) and (n!=5) and ((cost-n)>0):
+        if (n != 25) and (n!= 10) and (n!=5) and(cost- n >0):
             continue
         else:
             cost = cost - n
