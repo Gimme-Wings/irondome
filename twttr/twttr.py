3e806a3f547e3423e097722ad62aff2e947df6c3
Gimme-Wings <rgorelik2022@fau.edu>
2024-09-11 19:18:33 +0000

    Wed, Sep 11, 2024, 3:18 PM -04:00

--- twttr/twttr.py
+++ twttr/twttr.py
@@ -2,9 +2,12 @@ text = input("Input: ")
 length = len(text)
 while length > 0:
         for i in text:
-            if i == "a":
+            if (i == "a") or( i == "A") or (i=="i") or (i=="I") or (i == "o") or (i == "O") or (i == "u") or (i == "U") or (i == "e") or (i == "E") :
+                length = length -1
                 continue
             else:
                 print(f"{i}", end ='')
                 length = length - 1
-            
+
+print()
+
