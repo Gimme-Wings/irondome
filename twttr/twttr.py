428eaf213815eb19788299b0a20f5f50053a1189
Gimme-Wings <rgorelik2022@fau.edu>
2024-09-11 19:01:41 +0000

    Wed, Sep 11, 2024, 3:01 PM -04:00

--- twttr/twttr.py
+++ twttr/twttr.py
@@ -1,9 +1,8 @@
 text = input("Input: ")
-print(len(text))
+
 for i in range(len(text)):
-    for j in text:
-        if j == "a":
-            continue
-        else:
-            print(f"output {j}")
-        break
+    if j == "a":
+        continue
+    else:
+        print(f"output {j}")
+    break
