dbcca128af888aff6ae97247990bb5e97225c490
Gimme-Wings <rgorelik2022@fau.edu>
2024-09-11 19:10:45 +0000

    Wed, Sep 11, 2024, 3:10 PM -04:00

--- twttr/twttr.py
+++ twttr/twttr.py
@@ -1,10 +1,10 @@
 text = input("Input: ")
 length = len(text)
 while length > 0:
-    for i in text:
-        if i == "a":
-            continue
-        else:
-            print(f"{i}", end ='')
-            length = length - 1
-        break
+        for i in text:
+            if i == "a":
+                continue
+            else:
+                print(f"{i}", end ='')
+                length = length - 1
+            break
