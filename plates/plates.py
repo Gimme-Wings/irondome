fce9ac4fc80a98f8e4dff53078925e45a1a67c3c
Gimme-Wings <rgorelik2022@fau.edu>
2024-09-12 16:42:12 +0000

    Thu, Sep 12, 2024, 12:42 PM -04:00

--- plates/plates.py
+++ plates/plates.py
@@ -17,15 +17,15 @@ def letters(l, length):
     number_list = ("123456890")
     while length > 0:
         for numb in l:
-            if numb = 1 or 2 or 3 or 4 or 5 or 6 or 7 or 8 or 9 or 0:
-                    first_number = l.find(number_list)
-                
-                length = length -1
+            if numb == (1 or 2 or 3 or 4 or 5 or 6 or 7 or 8 or 9 or 0):
+                    first_number = l.find(numb)
 
-    if first_number <= 2:
-        return False
-    else:
-        return True
+            length = length -1
+
+            if first_number >= '2':
+                return False
+            else:
+                return True
 
 
 main()
