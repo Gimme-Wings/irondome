80622ad18145f6af63f25545a54f0d79cdf7c9c5
Gimme-Wings <rgorelik2022@fau.edu>
2024-09-12 11:22:14 +0000

    Thu, Sep 12, 2024, 7:22 AM -04:00

--- FAU/python/cop2220/week4/leap.py
+++ FAU/python/cop2220/week4/leap.py
@@ -1,6 +1,6 @@
 year = int(input("what year is it? "))
 
-if year (%4 == 0) or :
+if (year % 4 == 0) and (year % 100 == 0 and year %400 != 0):
     print("leap")
 else:
     print("not a leap year")
