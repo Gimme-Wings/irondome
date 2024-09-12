1efd82a1623379969c370f0b9a3da67a188edf54
Gimme-Wings <rgorelik2022@fau.edu>
2024-09-12 16:43:15 +0000

    Thu, Sep 12, 2024, 12:43 PM -04:00

--- plates/plates.py
+++ plates/plates.py
@@ -17,8 +17,8 @@ def letters(l, length):
     number_list = ("123456890")
     while length > 0:
         for numb in l:
-            if numb == (1 or 2 or 3 or 4 or 5 or 6 or 7 or 8 or 9 or 0):
-                    first_number = l.find(numb)
+            if numb.isdigit():
+                
 
             length = length -1
 
