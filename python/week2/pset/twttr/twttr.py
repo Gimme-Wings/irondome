93baac12b877b325cb0fca1da4337ce3a475b0c2
Gimme-Wings <rgorelik2022@fau.edu>
2024-09-12 15:46:14 +0000

    Thu, Sep 12, 2024, 11:46 AM -04:00

--- python/week2/pset/twttr/twttr.py
+++ python/week2/pset/twttr/twttr.py
@@ -1,11 +1,11 @@
 text = input("Input: ")
 length = len(text)
-my_list = list(text)
+text_list = list(text)
 vowels = ['a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u','U']
 
 print("Output: ", end ='')
 while length > 0:
-        for i in my_list:
+        for i in text_list:
             if i in vowels:
                 length = length -1
                 continue
