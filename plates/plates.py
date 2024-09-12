5da255b22ae44e6c54d2551c0152fad9d0b7c241
Gimme-Wings <rgorelik2022@fau.edu>
2024-09-12 16:13:00 +0000

    Thu, Sep 12, 2024, 12:13 PM -04:00

--- plates/plates.py
+++ plates/plates.py
@@ -9,14 +9,16 @@ def main():
 def is_valid(s):
     length = len(s)
     if 2<= length <=6:
-        letters(s)
+        letters(s, length)
     else:
         return False
 
-def letters(l):
+def letters(l, length):
     number_list = ['1', '2', '3', '4', '5', '6', '7','8', '9','0']
-    for i in l:
-        first_number = l.find(number_list)
+    while length > 0:
+        for i in l:
+            first_number = l.find(number_list)
+            length = length -1
     if first_number <= 2:
         return False
     else:
