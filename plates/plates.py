8360d88a868a8e7816f1454707ff3b731ebbc31b
Gimme-Wings <rgorelik2022@fau.edu>
2024-09-12 16:02:31 +0000

    Thu, Sep 12, 2024, 12:02 PM -04:00

--- plates/plates.py
+++ plates/plates.py
@@ -10,7 +10,7 @@ def is_valid(s):
     letters(s)
 
 def letters(l):
-    first_number = l.find('0' or '1' or '2' or '3' or '4' or '5')
+    first_number = l.find('0')
     print(first_number)
     if first_number > 2:
         return True
