622a4dd2f5961b73c599447506254a3d60e73bef
Gimme-Wings <rgorelik2022@fau.edu>
2024-09-15 11:26:18 +0000

    Sun, Sep 15, 2024, 7:26 AM -04:00

---
+++ FAU/python/cop2220/week4/nest.py
@@ -0,0 +1,21 @@
+number = 5
+# outer if condition is true
+if number >= 0:
+    if number == 0:
+        print('x')
+    else:
+        print('y')
+else:
+    print('smaller than 0')
+
+
+#outer if condition is false
+number =  -5
+if number>0:
+    if number == 0:
+        print('0')
+    else:
+        print('wrong')
+else:
+    print('less than')
+
