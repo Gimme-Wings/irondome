8bc2a86206d9d0b09dee7175aa25696f926d5c20
Gimme-Wings <rgorelik2022@fau.edu>
2024-09-15 17:41:13 +0000

    Sun, Sep 15, 2024, 1:41 PM -04:00

---
+++ continue.py
@@ -0,0 +1,10 @@
+for number in range(5):
+    if number ==2:
+        continue
+    else:
+        print(number)
+
+for n in range(10):
+    print(n)
+    continue
+    print('nothing got printed')#this will not be reeached continue restarts the iteration
