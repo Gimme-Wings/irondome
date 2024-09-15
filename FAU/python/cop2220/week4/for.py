b796948feeabcf211c6e285c6a0c733b6ad615f8
Gimme-Wings <rgorelik2022@fau.edu>
2024-09-15 11:50:37 +0000

    Sun, Sep 15, 2024, 7:50 AM -04:00

--- FAU/python/cop2220/week4/for.py
+++ FAU/python/cop2220/week4/for.py
@@ -10,4 +10,4 @@ for char in text:
 
 my_list = [24, 'apple', False, 10.5]
 for element in my_list:
-    print(element)
+    print(element) # elemtnt will take class of whatever it is made to iterate
