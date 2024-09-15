c3991c16483844e771ac88faf43870e5daed1cce
Gimme-Wings <rgorelik2022@fau.edu>
2024-09-15 17:33:34 +0000

    Sun, Sep 15, 2024, 1:33 PM -04:00

--- range.py
+++ range.py
@@ -3,5 +3,7 @@ for i in range(5,10,2):
 
 for i in range(5):
     print(i)
-for i in range(10,15):
+for i in range(10,15):#this is exclusive will stop at 14
+    print(i)
+for i in range(-5,4):
     print(i)
