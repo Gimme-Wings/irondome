3c149e4c24ccab39640cae2dc9f05c54a07f7c87
Gimme-Wings <rgorelik2022@fau.edu>
2024-09-16 17:22:31 +0000

    Mon, Sep 16, 2024, 1:22 PM -04:00

--- FAU/python/cop2220/week4/leap.py
+++ FAU/python/cop2220/week4/leap.py
@@ -4,3 +4,14 @@ if (year % 4 == 0) and (year % 100 == 0 and year %400 != 0):
     print("leap")
 else:
     print("not a leap year")
+birth_year = 1970
+leap_years_lived = 0
+reg_years_lived = 0
+for i in range(birth_year,2024):
+    if (i % 4 == 0) and (i % 100 == 0 and i %400 != 0):
+        leap_years_lived = leap_years_lived +1
+    else:
+        reg_yeras_lived = reg_years_lived +1
+
+print(leap_years_lived)
+print(reg_years_lived)
