6a9a82a18bf8c170773a2f0694b6bc8b1077a99d
Gimme-Wings <rgorelik2022@fau.edu>
2024-09-18 16:21:32 +0000

    Wed, Sep 18, 2024, 12:21 PM -04:00

--- names.py
+++ names.py
@@ -2,15 +2,16 @@ name_one = "a"
 name_two = "b"
 name_three = "c"
 age_one = 10
-age_two = 12
+age_two = 14
 age_three = 6
-
+length = 3
 name_list = [name_one, name_two, name_three]
 age_list = [age_one, age_two, age_three]
-for name in name_list:
-    for num in range(3):
-        for age in age_list:
-            if age >=0 and age <= 12:
-                print(f"{name[num]} is a child")
-            elif age >=13 and age <=19:
-                print(f"{name[num]} is a teen")
+for i in range(length):
+    name = name_list[i]
+    age = age_list[i]
+
+    if age > 0 and age <=12:
+        print(f"{name} is a child")
+    elif age >=13 and age <=18:
+        print(f"{name} is a teen")
