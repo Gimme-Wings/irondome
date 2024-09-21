8c11d5846d1bc7aa946453d89f0a59e110025a9f
Gimme-Wings <rgorelik2022@fau.edu>
2024-09-21 21:05:09 +0000

    Sat, Sep 21, 2024, 5:05 PM -04:00

--- FAU/python/cop2220/week5/rand.py
+++ FAU/python/cop2220/week5/rand.py
@@ -1,16 +1,17 @@
 def main():
-    b = int(input("num: "))
-    prime(b)
+    for i in range(1000):
+        b = i
+        is_prime(b)
 
 
-def prime(b):
+def is_prime(b):
     fact = 0
     for n in range (1,b+1):
         if (b%n ==0):
             fact = fact + 1
     if fact == 2:
-        print("prime")
+        print(f"{b} is prime")
     else:
-        print("not prime")
+        print(f"{b} is not prime")
 
 main()
