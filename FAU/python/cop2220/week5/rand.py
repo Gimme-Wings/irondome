c63b46b0434c3bc854ee616ef8f3d41532de7a5c
Gimme-Wings <rgorelik2022@fau.edu>
2024-09-21 20:59:30 +0000

    Sat, Sep 21, 2024, 4:59 PM -04:00

--- FAU/python/cop2220/week5/rand.py
+++ FAU/python/cop2220/week5/rand.py
@@ -1,10 +1,11 @@
 def main():
-    b = int(input("num"))
+    b = int(input("num: "))
     prime(b)
 
 
 def prime(b):
     for n in range (1,b+1):
-        print(n, n % 1)
+        if (b%n ==0):
+            print(n)
 
 main()
