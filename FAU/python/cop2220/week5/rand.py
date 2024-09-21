da53d8fd0f5a114b35ed83262c9e09187358a1be
Gimme-Wings <rgorelik2022@fau.edu>
2024-09-21 20:53:44 +0000

    Sat, Sep 21, 2024, 4:53 PM -04:00

--- FAU/python/cop2220/week5/rand.py
+++ FAU/python/cop2220/week5/rand.py
@@ -1,4 +1,11 @@
-import random
+def main():
+    b = int(input())
+    a=prime(b)
+    if a >25:
+        pass
+    else:
+        print(a)
 
-a = random.random
-print(a)
+def prime(b):
+    if b % b == 1 and b % 1 == b:
+        return b
