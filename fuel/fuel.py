a7796d4eb8b8e3b83ca9b881b1da6c7e0aa4ed50
Gimme-Wings <rgorelik2022@fau.edu>
2024-09-22 18:34:03 +0000

    Sun, Sep 22, 2024, 2:34 PM -04:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -1,13 +1,27 @@
 def main():
     value = get_int()
     tried(value)
-
+    
+def get_perc(a):
+    if a == 1:
+        print("100%")
+    elif a == .75:
+        print("75%")
+    elif a == .5:
+        print("50%")
+    elif a == .25:
+        print("25%")
+    elif a <= .01:
+        print("E")
 def get_int():
-    perc = input("Fraction: ")
+    perc = int(input("Fraction: "))
     return perc
 
-def tried(value):
+def ValueError(value):
     try:
         a = value[0]/value[2]
     except ValueError:
-        
+        get_int()
+    else:
+        get_perc(a)
+main()
