074fb264e378eef47694f5fb5dd19d1f8f5e793e
Gimme-Wings <rgorelik2022@fau.edu>
2024-09-22 11:24:56 +0000

    Sun, Sep 22, 2024, 7:24 AM -04:00

--- python/week3/get_int.py
+++ python/week3/get_int.py
@@ -5,10 +5,9 @@ def main():
 def get_int():
     while True:
         try:
-            x = int(input("whats x"))
+            x = int(input("whats x "))
         except ValueError:
             print("x is not an int")
         else:
-            break
-    return x
+            return x #this is equal to a break
 main()
