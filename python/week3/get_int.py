51e558f9bbb59094b989171c81b1eb3e9c7ccede
Gimme-Wings <rgorelik2022@fau.edu>
2024-09-22 11:33:37 +0000

    Sun, Sep 22, 2024, 7:33 AM -04:00

--- python/week3/get_int.py
+++ python/week3/get_int.py
@@ -1,11 +1,11 @@
 def main():
-    x = get_int()
+    x = get_int("whats x? ")
     print(f"x is {x}")
 
-def get_int():
+def get_int(prompt):#using prompt to know what to show not to confuse
     while True:
         try:
-            return int(input("whats x "))
+            return int(input(prompt))
         except ValueError:
             pass #just keep on going in the loop
             #print("x is not an int")
