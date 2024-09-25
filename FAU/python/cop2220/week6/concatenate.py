12b5d21c61d999568dfea50a1bcacbfb6444e9f0
Gimme-Wings <rgorelik2022@fau.edu>
2024-09-25 20:13:22 +0000

    Wed, Sep 25, 2024, 4:13 PM -04:00

---
+++ FAU/python/cop2220/week6/concatenate.py
@@ -0,0 +1,4 @@
+list1 = [1,2,"wer", 4.75]
+list2 = [3,"cer","point",10]
+list3 = list1+list2
+print(list3)
---
+++ FAU/python/cop2220/week6/slice.py
@@ -0,0 +1,5 @@
+list1 = [1,2,3,"wer",9.5,"@"]
+list2 = list1[1:3]
+print(list2)
+list3 = list1[0:len(list1)-1:2]#jumping by 2
+print(list3)
