f7e36e3584dfc45c2ba40f82998cbb97861f612d
Gimme-Wings <rgorelik2022@fau.edu>
2024-09-25 20:28:18 +0000

    Wed, Sep 25, 2024, 4:28 PM -04:00

--- FAU/python/cop2220/week6/findinlist.py
+++ FAU/python/cop2220/week6/findinlist.py
@@ -4,7 +4,8 @@ if 10.5 in list1:
 if "@" not in list1:
     print("9 not in it")
 cap = 0
-text = "I love napkins and noone can stop me"
-if ("o" in text):
-    cap = cap + 1
+text = "I lOve napkins and noone can stop me"
+for i in text:
+    if ("o" in i) or ("O" in i):
+        cap = cap + 1
 print(cap)
