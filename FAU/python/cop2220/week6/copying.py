abb7fdc9a73aba3a213e9874f66e5864ce31a009
Gimme-Wings <rgorelik2022@fau.edu>
2024-09-28 20:41:17 +0000

    Sat, Sep 28, 2024, 4:41 PM -04:00

--- FAU/python/cop2220/week6/copying.py
+++ FAU/python/cop2220/week6/copying.py
@@ -3,6 +3,6 @@ my_list2 = my_list1
 #not how to copy, its just a reference to list1
 print(my_list1)
 my_list2 = my_list1.copy()
-my_list1 = my_list1.append(100)
+my_list1.append(100)
 print(my_list2)
 print(my_list1)
