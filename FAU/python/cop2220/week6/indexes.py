3b037bbbf814e14656c03d0bcbe257d67279df09
Gimme-Wings <rgorelik2022@fau.edu>
2024-09-28 20:06:28 +0000

    Sat, Sep 28, 2024, 4:06 PM -04:00

---
+++ FAU/python/cop2220/week6/indexes.py
@@ -0,0 +1,18 @@
+numbers = []
+for i in range(10):
+    x = int(input())
+    numbers.append(x)
+print(f"{numbers} is the list")
+#to find position of a certain number
+def get_element (n,numbers):
+    result = []
+    indexes = []
+    for idx, e in enumerate(numbers):
+        if e == n:
+            result.append(e)
+            indexes.append(idx)
+    return result, indexes
+
+sub_list, indexed = get_element(2,numbers)
+print(f"{sub_list} is what was received and how many times it saw 2")
+print(f"{indexed} is position of 2")
